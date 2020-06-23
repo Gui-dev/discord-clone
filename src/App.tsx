3d3f923b70842c6adfa0ab5477932b0216426b6b
Gui-dev <adrianon10@gmail.com>
2020-06-23 16:32:26 -0300

    Configurando o grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import Layout from './components/Layout'
 import GlobalStyle from './styles/GlobalStyle'
 
 function App() {
-  
+
   return (
     <>
+      <Layout />
       <GlobalStyle />
       <h1>Hello World</h1>
     </>
---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './style'
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default ServerList
